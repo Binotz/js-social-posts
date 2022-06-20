d4413eff3d6a5ba6a55a4cc3b4f99cf4de3e6c3a
Binotz <binotto.riccardo@gmail.com>
2022-06-20 14:48:16 +0200

    Milestone 2 done

--- main.js
+++ main.js
@@ -64,3 +64,72 @@ const postsArray = [
         likes: 103
     },
 ];
+const postList = document.querySelector('.posts-list');
+
+//Inizio popolando la pagina 
+printAllPosts(postsArray);
+
+
+
+
+
+//---------------------------------------------
+//Functions
+//---------------------------------------------
+//Arguments:
+//postsArray: array con tutti i post
+//Stampa tutti i post
+function printAllPosts(postsArray){
+    for(let i=0; i < postsArray.length; i++){
+        const currentPost = postsArray[i]
+        // console.log(printSinglePost(currentPost));
+        printSinglePost(currentPost);
+    }
+}
+
+//Arguments:
+//post: singolo post, che va a popolare il template
+function printSinglePost(post){
+    //popolo il template: se c'è l'immagine del post, renderizzo il relativo div, altrimenti no 
+    const template= 
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.profilePic}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author}</div>
+                    <div class="post-meta__time">${post.date}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.text}</div>
+        <div class="post__image">
+            ${(post.postPic ? printPostPic(post.postPic) : '')}
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
+    //concateno i post a quelli già esistenti
+    postList.innerHTML += template;
+    
+}
+
+//Renderizzo il div dell'immagine del post
+//arguments: path dell'immagine
+function printPostPic(imgPath){
+    return `<img src="${imgPath}" alt=""></img>`
+}
